b77af3fbbab4649497bf14984aad2b12def997d0
r. ávila <avila.dev@outlook.com>
2024-11-23 12:40:46 -0300

    feat: implement usecase

--- lib/shop/products/service/product_service.ex
+++ lib/shop/products/service/product_service.ex
@@ -1,5 +1,5 @@
 defmodule Shop.Products.ProductService do
-  alias Shop.{Products, Products.Product}
+  alias Shop.Products
 
   def all do
     case Products.all() do
@@ -11,8 +11,30 @@ defmodule Shop.Products.ProductService do
   def show(params) do
     with {:ok, id} <- Shop.Validation.get_required_field(params, "id"),
          {:ok, _id} <- Shop.Validation.is_integer(id, "id"),
-         {:ok, product} <- Products.get(id) do
-      {:ok, Product.to_json!(product)}
+         {:ok, _product} = success <- Products.get(id) do
+      success
+    else
+      {:error, _reason} = failure -> failure
+    end
+  end
+
+  def create(params) do
+    Products.insert(params)
+  end
+
+  def update(params) do
+    with {:ok, product} <- Products.get(params),
+         {:ok, _patched} = success <- product |> Products.update(params) do
+      success
+    else
+      {:error, _reason} = failure -> failure
+    end
+  end
+
+  def delete(params) do
+    with {:ok, product} <- Products.get(params),
+         {:ok, _} <- Products.delete(product.id) do
+      {:ok}
     else
       {:error, _reason} = failure -> failure
     end
