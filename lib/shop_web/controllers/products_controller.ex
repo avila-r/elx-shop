e205345660ce22cfc257cb03570638b6a5876694
r. ávila <avila.dev@outlook.com>
2024-11-23 12:41:17 -0300

    feat: implement products controller

--- lib/shop_web/controllers/products_controller.ex
+++ lib/shop_web/controllers/products_controller.ex
@@ -1,15 +1,62 @@
 defmodule ShopWeb.ProductsController do
   use ShopWeb, :controller
 
-  def show do
+  alias ShopWeb.ErrorJSON
+  alias Shop.Products.Product
+  alias Shop.Products.ProductService
+
+  def show(conn, params) do
+    case ProductService.show(params) do
+      {:ok, product} ->
+        conn
+        |> json(product |> Product.to_json!())
+
+      {:error, reason} ->
+        conn
+        |> put_status(:not_found)
+        |> put_view(json: ErrorJSON)
+        |> render(:error, message: reason)
+    end
   end
 
-  def create do
+  def create(conn, params) do
+    case ProductService.create(params) do
+      {:ok, product} ->
+        conn
+        |> put_status(:created)
+        |> json(product |> Product.to_json!())
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:bad_request)
+        |> put_view(json: ErrorJSON)
+        |> render(:error, changeset: changeset)
+    end
   end
 
-  def update do
+  def update(conn, params) do
+    case ProductService.update(params) do
+      {:ok, patched} ->
+        conn |> json(patched |> Product.to_json!())
+
+      {:error, reason} ->
+        conn
+        |> put_status(:bad_request)
+        |> put_view(json: ErrorJSON)
+        |> render(:error, message: reason)
+    end
   end
 
-  def delete do
+  def delete(conn, params) do
+    case ProductService.delete(params) do
+      {:ok} ->
+        conn |> json(%{message: "successfully deleted"})
+
+      {:error, reason} ->
+        conn
+        |> put_status(:not_found)
+        |> put_view(json: ErrorJSON)
+        |> render(:error, message: reason)
+    end
   end
 end
